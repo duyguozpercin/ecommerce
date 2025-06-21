c619af33f7872f5eca7df74c1ef1866cbfa16788
duyguozpercin <duygucetind@gmail.com>
2025-06-22 02:52:23 +0300

    Refactor Home component to use async data fetching and improve loading state handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
-import { useEffect, useState } from 'react';
-
 interface Product {
-  id: string
-  title: string
+  id: string;
+  title: string;
 }
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
-useEffect(() => {
-  fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(data => setProducts(data.products))
-    .catch(error => console.log('Error fetching products:', error));
-}, []);
-  return 
-  <main>
-  <h1 className='text-center m-12'>Ecom website</h1>
-  {products.map((product: Product) => {
-    return <h2 key={product.id}>{product.title}</h2>;
-  })}
-</main>
+export default async function Home() {
+  let products = [];
+  let loading = true;
+  try {
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
+    products = data.products;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading = false;
+  }
+
+  if (loading) return <p>Loading....</p>;
+
+  return (
+    <main>
+      <h1 className='text-center m-12'>Ecom website</h1>
+      {products.map((product: Product) => {
+        return <h2 key={product.id}>{product.title}</h2>;
+      })}
+    </main>
+  );
 }
