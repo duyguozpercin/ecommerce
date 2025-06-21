ff1e355c0481810d1b0a45a87e065e22457fc423
duyguozpercin <duygucetind@gmail.com>
2025-06-22 02:36:46 +0300

    Fix product state initialization and update Home component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 
+interface Product {
+  id: string
+  title: string
+}
+
 export default function Home() {
-  const [prodocts, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 useEffect(() => {
   fetch('https://dummyjson.com/products')
     .then(res => res.json())
@@ -10,6 +15,9 @@ useEffect(() => {
 }, []);
   return 
   <main>
-    <h1 className="text-center m-12">Home Page</h1>
-    </main>
+  <h1 className='text-center m-12'>Ecom website</h1>
+  {products.map((product: Product) => {
+    return <h2 key={product.id}>{product.title}</h2>;
+  })}
+</main>
 }
