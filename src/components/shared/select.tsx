806795376e7c54335d8940923b77e34c57aab73d
duyguozpercin <duygucetind@gmail.com>
2025-06-29 23:15:16 +0300

    Refactor SelectField component to accept multiple options and improve type definitions

--- src/components/shared/select.tsx
+++ src/components/shared/select.tsx
@@ -1,8 +1,33 @@
-import {forwardRef} from 'react';
+import { forwardRef } from 'react';
 
 type SelectFieldProps = {
   label: string;
-  option: string;
-  name?: string;
+  options: string[];
+  name: string;
   error?: string;
-} & React.InputHTMLAttributes<HTMLSelectElement>;
+} & React.SelectHTMLAttributes<HTMLSelectElement>;
+
+const SelectField = forwardRef<HTMLSelectElement, SelectFieldProps>(
+  ({ label, options, name, error, ...rest }, ref) => (
+    <div className="flex flex-col">
+      <label htmlFor={name}>{label}</label>
+      <select
+        className="bg-stone-200" id={name}
+        name={name}
+        ref={ref}
+        {...rest}>
+        <option value="">Choose</option>
+        {options.map((opt) => (
+          <option key={opt} value={opt}>
+            {opt}
+          </option>
+        ))}
+      </select>
+      {error && <p className='text-red-500 text-sm'>{error}</p>}
+    </div>
+  )
+)
+
+SelectField.displayName = 'SelectField';
+
+export default SelectField;
