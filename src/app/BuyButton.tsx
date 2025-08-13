ae7e2ede508b59298645627510847290eac7ec5b
duyguozpercin <duygucetind@gmail.com>
2025-08-13 16:45:57 +0300

    refactor: clean up whitespace and improve formatting in BuyButton component

--- src/app/BuyButton.tsx
+++ src/app/BuyButton.tsx
@@ -1,35 +1,20 @@
 'use client';
 
-
-
 export const BuyButton = ({
-
-productId,
-
-className,
-
+  productId,
+  className,
 }: { productId: string; className?: string }) => {
 
-return (
+  return (
 
-<form action="/api/checkout_session" method="POST" onClick={(e) => e.stopPropagation()}>
+    <form action="/api/checkout_session" method="POST" onClick={(e) => e.stopPropagation()}>
+      <input type="hidden" name="productId" value={productId} />
 
-<input type="hidden" name="productId" value={productId} />
+      <button
+        type="submit"
+        className={`bg-[#c6937b] text-white text-sm px-2 py-1 rounded hover:bg-amber-600 cursor-pointer transition-colors duration-300 leading-normal z-10 ${className ?? ""}`}>
+        Buy Now </button>
 
-<button
-
-type="submit"
-
-className={`bg-[#c6937b] text-white text-sm px-2 py-1 rounded hover:bg-amber-600 cursor-pointer transition-colors duration-300 leading-normal z-10 ${className ?? ""}`}
-
->
-
-Buy Now
-
-</button>
-
-</form>
-
-);
-
+    </form>
+  );
 };
