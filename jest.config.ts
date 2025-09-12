fbf8843995af07bfd7388f6fce63309883725490
duyguozpercin <duygucetind@gmail.com>
2025-09-12 22:09:39 +0300

    fix: update import formatting and add tests for ProductsClientTable component

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 import nextJest from 'next/jest';
 
@@ -101,10 +101,9 @@ const customJestConfig = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+ 
   moduleNameMapper: {
-
     '^@/(.*)$': '<rootDir>/src/$1',
-
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
