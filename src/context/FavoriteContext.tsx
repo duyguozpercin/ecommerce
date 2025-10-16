521c9ede2802651bc472474d00d701b939f9820c
duyguozpercin <duygucetind@gmail.com>
2025-10-16 16:56:57 +0300

    feat: add FavoriteContext and FavoritesProvider for managing favorite products

---
+++ src/context/FavoriteContext.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, { createContext, useState } from "react";
+
+interface FavoriteContextType {
+  favorites: string[];
+  toggleFavorite: (productId: string) => void;
+  isFavorite: (productId: string) => boolean;
+}
+
+const FavoriteContext = createContext<FavoriteContextType | undefined>(undefined);
+
+const FavoritesProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [favorites, setFavorites] = useState<string[]>([]);
+
+  function toggleFavorite(productId: string) {
+    setFavorites((prevFavorites) => {
+      if (prevFavorites.includes(productId)) {
+        return prevFavorites.filter((id) => id !== productId);
+      }
+      // ensure we always return a string[] (no undefined)
+      return [...prevFavorites, productId];
+    });
+  }
+
+  function isFavorite(productId: string) {
+    return favorites.includes(productId);
+  }
+
+  return (
+    <FavoriteContext.Provider value={{ favorites, toggleFavorite, isFavorite }}>
+      {children}
+    </FavoriteContext.Provider>
+  );
+};
+
+export { FavoriteContext, FavoritesProvider };
